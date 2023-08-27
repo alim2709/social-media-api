8de9990c5b66fb8dd853da270da6f42c174e16fb
alim2709 <alimselemetow@gmail.com>
2023-08-28 00:04:42 +0300

    Add ProfileViewSet to user/views.py

--- user/views.py
+++ user/views.py
@@ -1,9 +1,69 @@
-from rest_framework import generics
+from django.db import IntegrityError
+from rest_framework import generics, viewsets, status
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-from user.serializers import UserSerializer
+from user.models import Profile
+from user.serializers import (
+    UserSerializer,
+    ProfileListSerializer,
+    ProfileDetailSerializer,
+    ProfileSerializer,
+    ProfilePictureSerializer,
+)
+
+
+# ProfileListSerializer)
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ProfileListSerializer
+        if self.action == "retrieve":
+            return ProfileDetailSerializer
+        if self.action == "upload_picture":
+            return ProfilePictureSerializer
+        return ProfileSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="follow_unfollow",
+    )
+    def follow_unfollow(self, request, pk=None):
+        """Endpoint for following & unfollowing profile"""
+        profile = self.get_object()
+        user = self.request.user
+        if profile.followers.filter(pk=user.pk).exists():
+            profile.followers.remove(user)
+            user.profile.following.remove(profile.user)
+            return Response({"status": "unfollow"})
+        profile.followers.add(user)
+        user.profile.following.add(profile.user)
+        return Response({"status": "follow"})
+
+    @action(
+        methods=["POST", "DELETE"],
+        detail=True,
+        url_path="upload-picture",
+    )
+    def upload_picture(self, request, pk=None):
+        """Endpoint for uploading picture to specific profile"""
+        profile = self.get_object()
+        serializer = self.get_serializer(profile, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
