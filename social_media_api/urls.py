a84ec3f4f23e2c61a91ca48acd040d7a24a7c568
alim2709 <alimselemetow@gmail.com>
2023-09-05 00:25:26 +0300

    Add path to social_network app in urls.py

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -25,6 +25,10 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
+    path(
+        "api/social_network/",
+        include("social_network.urls", namespace="social_network"),
+    ),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
