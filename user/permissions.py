361c7696bbba8b3aefb329d3c2602f61a7170cc2
alim2709 <alimselemetow@gmail.com>
2023-09-04 14:24:58 +0300

    Implement IsUserHaveProfile permission in permissions.py

--- user/permissions.py
+++ user/permissions.py
@@ -1,9 +1,15 @@
 from rest_framework import permissions
 
+from user.models import Profile
+
 
 class IsOwnerOrIsAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-
         return obj.user == request.user or request.user.is_staff
+
+
+class IsUserHaveProfile(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return Profile.objects.filter(user=request.user).exists()
